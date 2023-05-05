4c7faf52b7d72614e68148fa87950ec2a3be5923
Miguel Aguilera <superbrandon2018@gmail.com>
2023-05-05 14:43:16 -0500

    Change Database

--- build/database.js
+++ build/database.js
@@ -7,7 +7,7 @@ const mysql_1 = __importDefault(require("mysql"));
 const db = mysql_1.default.createPool({
     host: "b2e2w0o0ppbm09xyr5ys-mysql.services.clever-cloud.com",
     user: "ufmmsgqyefsctovu",
-    password: "12DFh7xisGnbUHGTBxVxUE3456789",
+    password: "DFh7xisGnbUHGTBxVxUE",
     database: "b2e2w0o0ppbm09xyr5ys",
     port: 3306,
 });
--- database.ts
+++ database.ts
@@ -3,7 +3,7 @@ import mysql from 'mysql'
 const db = mysql.createPool({
     host: "b2e2w0o0ppbm09xyr5ys-mysql.services.clever-cloud.com",
     user:"ufmmsgqyefsctovu",
-    password:"12DFh7xisGnbUHGTBxVxUE3456789",
+    password:"DFh7xisGnbUHGTBxVxUE",
     database: "b2e2w0o0ppbm09xyr5ys",
     port:3306,
 })
