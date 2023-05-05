a623a2f7565ca05884dd7639ae451291f2b452c6
Miguel Aguilera <superbrandon2018@gmail.com>
2023-05-05 12:12:58 -0500

    Change Database to Local

--- build/database.js
+++ build/database.js
@@ -5,10 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql_1 = __importDefault(require("mysql"));
 const db = mysql_1.default.createPool({
-    host: "us-cdbr-east-04.cleardb.com",
-    user: "b977b2dec65957",
-    password: "25068e20",
-    database: "heroku_c9a186fb0967e5b",
+    host: "localhost",
+    user: "root",
+    password: "123456789",
+    database: "Proactive",
     port: 3306,
 });
 exports.default = db;
